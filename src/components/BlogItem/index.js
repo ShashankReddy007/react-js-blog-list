b1c7cf80943c40b3339c939977cbf7c9dee34f69
Shashank Reddy <shashank.godala@gmail.com>
2023-03-20 17:36:56 -0700

    second-commit

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,1 +1,19 @@
-// Write your JS code here
+import './index.css'
+
+const BlogItem = props => {
+  const {item} = props
+  const {title, description, publishedDate} = item
+
+  return (
+    <li className="contact-container">
+      <div className="title-date">
+        <p className="title">{title}</p>
+        <p className="date">{publishedDate}</p>
+      </div>
+      <p className="description">{description}</p>
+      <hr className="horizontal-line" />
+    </li>
+  )
+}
+
+export default BlogItem
--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,1 +1,17 @@
-// Write your JS code here
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const BlogList = props => {
+  const {blogsList} = props
+  const {id, title, description, publishedDate} = blogsList
+
+  return (
+    <ul className="list-container">
+      {blogsList.map(item => (
+        <BlogItem item={item} key={item.id} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
